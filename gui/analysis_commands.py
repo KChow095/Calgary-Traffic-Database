ab1415460e8bfe977acf6335cdea78c89c8a8ad2
PeiyunZhao <64915482+PeiyunZhao@users.noreply.github.com>
2020-07-18 11:47:51 -0600

    GUI functional with displaying figures

--- gui/analysis_commands.py
+++ gui/analysis_commands.py
@@ -1,52 +1,89 @@
 from tkinter import *
 from tkinter import ttk
 
-def read_func(type_,year_):
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+def read_msg(type_,year_):
     if type_=="" and year_=="":
-        return 'please enter: \ntype and year to read'
+        return 'please enter: \ntype and year to read' , 'red'
     elif type_ =="":
-        return 'please enter: \ntype to read'
+        return 'please enter: \ntype to read' ,'red'
     elif year_=="":
-        return 'please enter: \nyear to read'
+        return 'please enter: \nyear to read','red'
     else:
-        return 'Read '+type_+" "+year_
+        return 'Read '+type_+" "+year_ ,'green'
+
     
+
+def sort_msg(type_,year_):
+
+    if type_=="" and year_=="":
+        return 'please enter: \ntype and year to sort','red'
+    elif type_ =="":
+        return 'please enter: \ntype to sort' ,'red'
+    elif year_=="":
+        return 'please enter: \nyear to sort' , 'red'
+    else:
+        return 'Sort '+type_+" "+year_, 'green'
+    
+    
+
+def analysis_msg(type_,year_):
+
+    if type_=="" and year_=="":
+        return 'please enter: \ntype and year to analyze','red'
+    elif type_ =="":
+        return 'please enter: \ntype to analyze','red'
+    elif year_=="":
+        return 'please enter: \nyear to analyze' , 'red'
+    else:
+        return 'Analyze '+type_+" "+year_ , 'green'
+    
+    
+
+def map_msg (type_,year_):
+    
+    if type_=="" and year_=="":
+        return 'please enter: \ntype and year to map','red'
+    elif type_ =="":
+        return 'please enter: \ntype to map','red'
+    elif year_=="":
+        return 'please enter: \nyear to map','red'
+    else:
+        return 'Map '+type_+" "+year_,'green'
+    
+    
+
+def read_func(type_,year_):
+    pass
+
 
 def sort_func(type_,year_):
-
-    if type_=="" and year_=="":
-        return 'please enter: \ntype and year to sort'
-    elif type_ =="":
-        return 'please enter: \ntype to sort'
-    elif year_=="":
-        return 'please enter: \nyear to sort'
-    else:
-        return 'Sort '+type_+" "+year_
-    
-    
+    pass
+        
 
 def analysis_func(type_,year_):
 
-    if type_=="" and year_=="":
-        return 'please enter: \ntype and year to analyze'
-    elif type_ =="":
-        return 'please enter: \ntype to analyze'
-    elif year_=="":
-        return 'please enter: \nyear to analyze'
-    else:
-        return 'Analyze '+type_+" "+year_
-    
+    # TO be Removed and replaced with database plot, must return figure to plot
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
+
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
+
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+        title='About as simple as it gets, folks')
+
+    return fig 
     
 
+
+values = np.arange(0, 2500, 500)
+value_increment = 1000
+
+
 def map_func(type_,year_):
-    
-    if type_=="" and year_=="":
-        return 'please enter: \ntype and year to map'
-    elif type_ =="":
-        return 'please enter: \ntype to map'
-    elif year_=="":
-        return 'please enter: \nyear to map'
-    else:
-        return 'Map '+type_+" "+year_
-    
+    pass
     
--- gui/main.py
+++ gui/main.py
@@ -1,84 +1,162 @@
 from tkinter import *
 from tkinter import ttk
 from analysis_commands import *
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
-#window design
-root = Tk()
-root.title('Calgary Traffic Database')
-root.geometry("1000x500")
+import numpy as np
+import matplotlib.pyplot as plt
 
-left = Frame(root, width=150, borderwidth=1, relief="solid")
-right = Frame(root, width=750, borderwidth=1, relief="solid")
-Status = Frame(left, borderwidth=1, relief="solid")
+def main():
+    #window design
+    root = Tk()
+    root.title('Calgary Traffic Database')
+    root.geometry("1000x600")
 
-Year_values=['2016','2017','2018']
-Type_values=['Accidents','Traffic Volume']
+    left = Frame(root, width=150, borderwidth=1, relief="solid")
+    right = Frame(root, width=750, borderwidth=1, relief="solid")
+    global right_canvas
+    right_canvas = Frame(right)
 
-Type_label = Label(left, text="Type",anchor=W,justify=LEFT)
-CB_type=ttk.Combobox(left, justify=LEFT,value= Type_values)
 
-Year_label = Label(left, text="Year",anchor=W,justify=LEFT)
-CB_year=ttk.Combobox(left, justify=LEFT,value= Year_values)
+    Status = Frame(left, borderwidth=1, relief="solid")
 
-Status_out=Label(Status)
+    Year_values=['2016','2017','2018']
+    Type_values=['Accidents','Traffic Volume']
 
-Button_read=Button(left,text="Read", padx=50,pady=10,command=lambda: read_click(CB_type.get(),CB_year.get()))
-Button_sort=Button(left,text="Sort", padx=50,pady=10,command=lambda:  sort_click(CB_type.get(),CB_year.get()))
-Button_analysis=Button(left,text="Analysis",padx=50,pady=10,command=lambda: analysis_click(CB_type.get(),CB_year.get()))
-Button_map=Button(left,text="Map", padx=50,pady=10,command=lambda: view_map_click(CB_type.get(),CB_year.get()))
+    #creat Type and Year ComboBox
+    Type_label = Label(left, text="Type",anchor=W,justify=LEFT)
+    CB_type=ttk.Combobox(left, justify=LEFT,value= Type_values)
 
-def read_click(type_,year_):
-
-    #Calls function and updates Status
+    Year_label = Label(left, text="Year",anchor=W,justify=LEFT)
+    CB_year=ttk.Combobox(left, justify=LEFT,value= Year_values)
+    
     global Status_out
-    Status_out.destroy()
-    Message = read_func(type_,year_)
-    Status_out=Label(Status,text=Message,anchor=W,justify=LEFT)
-    Status_out.pack(fill='x')
+    Status_out=Label(Status)
 
-def sort_click(type_,year_):
+    #Button click commands
 
-    #Calls function and updates Status
-    global Status_out
-    Status_out.destroy()
-    Message= sort_func(type_,year_)
-    Status_out=Label(Status,text=Message,anchor=W,justify=LEFT)
-    Status_out.pack(fill='x')
+    def read_click(type_,year_):
 
-def analysis_click(type_,year_):
+        #updates Status
+        global Status_out
+        Status_out.destroy()
+        Message ,Colour = read_msg(type_,year_)
+        Status_out=Label(Status,text=Message,anchor=W,justify=LEFT,background=Colour)
+        Status_out.pack(fill='x')
 
-    #Calls function and updates Status
-    global Status_out
-    Status_out.destroy()
-    Message= analysis_func(type_,year_)
-    Status_out=Label(Status,text=Message,anchor=W,justify=LEFT)
-    Status_out.pack(fill='x')
+        #Calls function
+        if Colour == 'green':
+            global right_canvas
+            right_canvas.destroy()
 
-def view_map_click(type_,year_):
+            right_canvas=Frame(right,width=750,background='green')
+            right_canvas.pack(expand=True,fill="both")
 
-    #Calls function and updates Status
-    global Status_out
-    Status_out.destroy()
-    Message = map_func(type_,year_)
-    Status_out=Label(Status,text=Message,anchor=W,justify=LEFT)
-    Status_out.pack(fill='x')
+            pass
 
-Status_label = Label(left, text="Status:",anchor=W,justify=LEFT)
 
-Type_label.pack(fill='x')
-CB_type.pack(fill='x')
-Year_label.pack(fill='x')
-CB_year.pack(fill='x')
+    def sort_click(type_,year_):
 
-Button_read.pack(fill='x')
-Button_sort.pack(fill='x')
-Button_analysis.pack(fill='x')
-Button_map.pack(fill='x')
+        #updates Status
+        global Status_out
+        Status_out.destroy()
+        Message, Colour,= sort_msg(type_,year_)
+        Status_out=Label(Status,text=Message,anchor=W,justify=LEFT,background=Colour)
+        Status_out.pack(fill='x')
 
-Status_label.pack(fill='x')
-Status.pack(expand=True, fill="both", padx=5, pady=5)
+        #Calls function
+        if Colour == 'green':
+            global right_canvas
+            right_canvas.destroy()
 
-left.pack(side="left", expand=True, fill="both")
-right.pack(side="right", expand=True, fill="both")
+            right_canvas=Frame(right,width=750,background='green')
+            right_canvas.pack(expand=True,fill="both")
 
-root.mainloop()
+            pass
+
+
+    def analysis_click(type_,year_):
+
+        #updates Status
+        global Status_out
+        Status_out.destroy()
+        Message , Colour= analysis_msg (type_,year_)
+        Status_out=Label(Status,text=Message,anchor=W,justify=LEFT,background=Colour)
+        Status_out.pack(fill='x')
+
+        #Calls function
+        if Colour == 'green':
+            #removes previous right_canvas
+            global right_canvas
+            right_canvas.destroy()
+            right_canvas=Frame(right,width=750,background='green')
+
+            f=analysis_func(type_,year_)
+            canvas= FigureCanvasTkAgg(f,right_canvas)
+            canvas.draw()
+            canvas.get_tk_widget().pack(side=TOP, fill = BOTH, expand=True)
+
+            toolbar=NavigationToolbar2Tk(canvas,right_canvas)
+            toolbar.update()
+            canvas._tkcanvas.pack(side=TOP, fill = BOTH, expand=True)
+
+            #updates to new right_canvas
+            
+            right_canvas.pack(expand=True,fill="both")
+
+
+
+
+    def map_click(type_,year_):
+
+        #updates Status
+        global Status_out
+        Status_out.destroy()
+        Message , Colour = map_msg(type_,year_)
+        Status_out=Label(Status,text=Message,anchor=W,justify=LEFT,background=Colour)
+        Status_out.pack(fill='x')
+
+        #Calls function
+        if Colour == 'green':
+            global right_canvas
+            right_canvas.destroy()
+
+            right_canvas=Frame(right,width=750,background='green')
+            right_canvas.pack(expand=True,fill="both")
+
+            pass
+
+    #Create Buttons
+    Button_read=Button(left,text="Read", padx=50,pady=10,command=lambda: read_click(CB_type.get(),CB_year.get()))
+    Button_sort=Button(left,text="Sort", padx=50,pady=10,command=lambda:  sort_click(CB_type.get(),CB_year.get()))
+    Button_analysis=Button(left,text="Analysis",padx=50,pady=10,command=lambda: analysis_click(CB_type.get(),CB_year.get()))
+    Button_map=Button(left,text="Map", padx=50,pady=10,command=lambda: map_click(CB_type.get(),CB_year.get()))
+
+    #display Combobox and header labels
+    Type_label.pack(fill='x')
+    CB_type.pack(fill='x')
+    Year_label.pack(fill='x')
+    CB_year.pack(fill='x')
+
+    #display function buttons
+    Button_read.pack(fill='x')
+    Button_sort.pack(fill='x')
+    Button_analysis.pack(fill='x')
+    Button_map.pack(fill='x')
+
+    #display status box
+    Status_label = Label(left, text="Status:",anchor=W,justify=LEFT)
+    Status_label.pack(fill='x')
+    Status.pack(expand=True, fill="both", padx=5, pady=5)
+
+    #display left and right frames
+    left.pack(side="left", expand=True, fill="both")
+    right.pack(side="right", expand=True, fill="both")
+
+    root.mainloop()
+
+
+
+if __name__ == "__main__":
+    main()
+
